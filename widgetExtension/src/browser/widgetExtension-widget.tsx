fdde4d867fbdf00c70a7d8cd6f2ab913d2312d51
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2025-01-13 15:11:17 -0500

    Added save support.

--- widgetExtension/src/browser/widgetExtension-widget.tsx
+++ widgetExtension/src/browser/widgetExtension-widget.tsx
@@ -2,14 +2,39 @@ import * as React from 'react';
 import { injectable, postConstruct, inject } from '@theia/core/shared/inversify';
 import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
-import { MessageService } from '@theia/core';
-import { Message } from '@theia/core/lib/browser';
+import {Emitter, Event, MessageService} from '@theia/core';
+import {Message, Saveable} from '@theia/core/lib/browser';
+import { BinaryBuffer } from '@theia/core/lib/common/buffer';
 
 @injectable()
-export class WidgetExtensionWidget extends ReactWidget {
+export class WidgetExtensionWidget extends ReactWidget implements Saveable {
+    public readonly onContentChangedEmitter = new Emitter<void>();
+    onContentChanged: Event<void> = this.onContentChangedEmitter.event;
+    revert?(options?: Saveable.RevertOptions): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    createSnapshot?(): Saveable.Snapshot {
+        throw new Error('Method not implemented.');
+    }
+    applySnapshot?(snapshot: object): void {
+        throw new Error('Method not implemented.');
+    }
+    serialize?(): Promise<BinaryBuffer> {
+        throw new Error('Method not implemented.');
+    }
 
     static readonly ID = 'widgetExtension:widget';
+    dirty: boolean= false;
     static readonly LABEL = 'WidgetExtension Label';
+    public readonly onDirtyChangedEmitter = new Emitter<void>();
+    onDirtyChanged: Event<void> = this.onDirtyChangedEmitter.event
+    autoSave: "off";
+
+    save(): import ("@theia/core").MaybePromise<void> {
+        //Save your content 
+        this.dirty=false;
+        this.onDirtyChangedEmitter.fire(undefined);
+    }
 
     @inject(MessageService)
     protected readonly messageService!: MessageService;
@@ -22,7 +47,7 @@ export class WidgetExtensionWidget extends ReactWidget {
     protected async doInit(): Promise <void> {
         this.id = WidgetExtensionWidget.ID;
         this.title.label = WidgetExtensionWidget.LABEL;
-        this.title.caption = WidgetExtensionWidget.LABEL;
+        this.title.caption = `Caption ${WidgetExtensionWidget.LABEL}`;
         this.title.closable = true;
         this.title.iconClass = 'fa fa-window-maximize'; // example widget icon.
         this.update();
@@ -38,6 +63,8 @@ export class WidgetExtensionWidget extends ReactWidget {
     }
 
     protected displayMessage(): void {
+        this.dirty=true;
+        this.onDirtyChangedEmitter.fire(undefined);
         this.messageService.info('Congratulations: WidgetExtension Widget Successfully Created!');
     }
 
